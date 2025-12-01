f086d051c39dd70ffc3cbf6e8d4288524c9b3e88
seoyeoning <minalunpark@naver.com>
2025-12-01 09:31:00 +0900

    init

---
+++ src/main/java/ivory/ivory_be/IvoryBeApplication.java
@@ -0,0 +1,13 @@
+package ivory.ivory_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvoryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ivory/ivory_be/IvoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ivory.ivory_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvoryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
