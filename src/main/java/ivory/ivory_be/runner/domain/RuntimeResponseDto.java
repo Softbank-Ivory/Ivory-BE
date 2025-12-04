1550e20da2efc980fba3e17067c8d8567a978663
성시훈 <tjdtlgns99@naver.com>
2025-12-04 21:32:52 +0900

    fix: 런타임 DTO 수정

--- src/main/java/ivory/ivory_be/runner/domain/RuntimeResponseDto.java
+++ src/main/java/ivory/ivory_be/runner/domain/RuntimeResponseDto.java
@@ -1,6 +1,13 @@
 package ivory.ivory_be.runner.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class RuntimeResponseDto {
-    public RuntimeResponseDto(String name, String runtime) {
-    }
+    private String name;
+    private String version;
 }
